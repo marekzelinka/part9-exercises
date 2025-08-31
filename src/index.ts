d6e52630e8b252c74383be983f858c1b7a65cee8
Marek Zelinka <mzelinka17@gmail.com>
2025-08-31 14:21:19 +0200

    solve 9.5 WebBMI

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { calculateBmi } from "./utils.ts";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,26 @@ app.get("/ping", (_req, res) => {
 
 app.get("/hello", (_req, res) => {
 	res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+	const height = Number(req.query.height);
+	const weight = Number(req.query.weight);
+
+	if (Number.isNaN(height) || Number.isNaN(weight)) {
+		res.status(400).json({
+			error: "malformatted parameters",
+		});
+		return;
+	}
+
+	const result = calculateBmi(height, weight);
+
+	res.json({
+		weight,
+		height,
+		bmi: result,
+	});
 });
 
 app.listen(PORT, () => {
